89f7c2210013c7f2cb0d7226790f8c1b8676acd9
Anton Brekhov <tony.brekhov@ya.ru>
2018-03-11 20:22:58 +0300

    Copied modified version of client from hw2 branch after second request

--- HW3/client.pl
+++ HW3/client.pl
@@ -3,23 +3,13 @@
 use Getopt::Long;
 use Pod::Usage;
 use Term::ReadLine;
+use warnings;
+use strict;
 use 5.016;
 
 
 #Options
 my $verbose='';
-my $help='';
-my @args;
-Getopt::Long::Configure('bundling');
-GetOptions(
-	'v|verbose+'=>\$verbose,
-	'h|help'=>\$help
-);
-
-say "Verbose level: $verbose" if $verbose ;
-#Help init
-say "Initialization of help..." if $verbose;
-
 my $instructions = <<EOF;
 Usage:
 	client.pl [-h] [-v] /path/to/somewhere
@@ -27,7 +17,14 @@ Usage:
 	-v | --verbose - be verbose	
 EOF
 
-die $instructions if $help;
+
+Getopt::Long::Configure('bundling');
+GetOptions(
+	'v|verbose+'=>\$verbose,
+	'h|help'=>sub{ die($instructions); }
+);
+
+say "Verbose level: $verbose" if $verbose ;
 
 #Absolute path init
 say "Arguments:[".join(',',@ARGV)."]" if $verbose;
@@ -37,32 +34,25 @@ chomp $currpath;
 say "Absolute path:".$currpath."..." if $verbose;
 
 #Commands functions
-sub ls(){
+sub ls{
 	say "debug: List of files(ls)" if $verbose>0;
-	say "debug2: qx(cd $currpath && ls -lA)" if $verbose>1;
-	return qx(cd $currpath && ls -lA);
+	say "debug2: qx(ls -lA $currpath)" if $verbose>1;
+	return qx(ls -lA $currpath);
 }
-sub rm(){
-	my @args = @_;
-	say "debug: Removing $args[1](rm)" if $verbose>0;
-	say "debug2: unlink $currpath/$args[1]" if $verbose>1;
-	unlink $currpath."/".$args[1]; 
+sub rm{
+	say "debug: Removing $_[1](rm)" if $verbose>0;
+	say "debug2: unlink $currpath/$_[1]" if $verbose>1;
+	unlink $currpath."/".$_[1]; 
 }
-sub mv(){
-	my @args = @_;
-	say "debug: Renaming $args[1] in $args[2] (mv)" if $verbose>0;
-	say "debug2: rename currpath/$args[1] , $currpath/$args[2]" if $verbose>1;
-	rename $currpath."/".$args[1] , $currpath."/".$args[2];
+sub mv{
+	say "debug: Renaming $_[1] in $_[2] (mv)" if $verbose>0;
+	say "debug2: rename currpath/$_[1] , $currpath/$_[2]" if $verbose>1;
+	rename $currpath."/".$_[1] , $currpath."/".$_[2];
 }
-sub cp(){
-	my @args = @_;
-	say "debug: Copying $args[1] in $currpath/$args[2] (cp)" if $verbose>0;
-	say "debug2: qx(cp $args[1] $currpath/$args[2])" if $verbose>1;
-	return qx(cp $args[1] $currpath/$args[2]);
-}
-sub exit(){
-	say "Exitting client..." if $verbose>0;
-	return eval("exit");
+sub cp{
+	say "debug: Copying $_[1] in $currpath/$_[2] (cp)" if $verbose>0;
+	say "debug2: qx(cp $_[1] $currpath/$_[2])" if $verbose>1;
+	return qx(cp $_[1] $currpath/$_[2]);
 }
 
 #Commands init
@@ -71,42 +61,44 @@ my %commands = (
 	'cp'=>\&cp,
 	'mv'=>\&mv,
 	'rm'=>\&rm,
-	'exit'=>\&exit
 );
 
 my $term = Term::ReadLine->new('Perl local client ');
 my $prompt = "> ";
-$term->Features->{autohistory}=1;
+#$term->Features->{autohistory}=1;
 $term->read_history();
 my $attribs = $term->Attribs;
 $attribs->{completion_entry_function} = $attribs->{list_completion_function};
-$attribs->{completion_word}=[qw(ls cp mv rm exit)];
+$attribs->{completion_word}=[keys %commands];
 my $OUT = $term->OUT || \*STDOUT;
 while ( defined ($_ = $term->readline($prompt)) ) {
-	$term->write_history() if /\S/;
+
 	my $res="";
-	my @args = split /\s+/, $_;
-	#say join ",", @args;
-	if ($_ =~/^\!(.*)/){
+	if (/^\!(.*)/){
 		$res= qx($1);
-		warn $@ if $@;
 	}
-	elsif($_ =~/^$/){
+	elsif(/^$/){
 		$res = $_;
 	}
+	elsif(/^exit$/){
+		say "Exitting client..." if $verbose>0;
+		exit;
+	}
 	else{
+		my @args = split /\s+/, $_;
 		eval{
 			$res = $commands{$args[0]}->(@args); 	
-		 };
-		warn "This version support only: ls, cp ,mv, rm functions." if $@;
+		 } or say "This version support only: ".join(", ",keys %commands)." functions.";
 		
 
 	}
 	say $OUT $res unless $@;
-	$term->addhistory($_) if /\S/;
+	#$term->addhistory($_) if /\S/;
 
 }
 
-
+END{
+	$term->write_history();
+}
 
 
